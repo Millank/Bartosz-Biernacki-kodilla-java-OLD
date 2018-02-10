6a486aa3cd5b1c379df479f2744a7136961202fd
Millank <32596365+Millank@users.noreply.github.com>
2018-02-10 13:00:09 +0100

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/User/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/User/SimpleUser.java
@@ -1,20 +1,4 @@
 package com.kodilla.testing.User;
 
-/**
- * Created by Bartosz Biernacki.
- */
-
 public class SimpleUser {
-    String username;
-    String realName;
-    public SimpleUser(String username, String realName) {
-        this.username = username;
-        this.realName = realName;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getRealName(){
-        return realName;
-    }
 }
