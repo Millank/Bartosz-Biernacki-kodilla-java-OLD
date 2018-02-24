4a6eff7a42b9a538242151d872bc5d2c3eb779d1
Millank <32596365+Millank@users.noreply.github.com>
2018-02-24 19:12:52 +0100

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,13 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
 
 public class StreamMain {
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
-      //  System.out.println("Calculating expressions with lambdas");
+        //  System.out.println("Calculating expressions with lambdas");
         //expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
         //expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
         //expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
@@ -20,14 +19,18 @@ public class StreamMain {
         //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-        
+
         // Zadanie 7.1
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("XYZ",  (text) -> text + "ABC" );
-        poemBeautifier.beautify("talk capslock",  (text) -> text.toUpperCase() );
-        poemBeautifier.beautify("XYZ",  (text) -> text.toLowerCase() );
-        poemBeautifier.beautify("123456789",  (text) -> text.substring(3) );
+        poemBeautifier.beautify("XYZ", (text) -> text + "ABC");
+        poemBeautifier.beautify("talk capslock", (text) -> text.toUpperCase());
+        poemBeautifier.beautify("XYZ", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("123456789", (text) -> text.substring(3));
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        com.kodilla.stream.iterate.NumbersGenerator.generateEven(20);
     }
+
 }
+
